da1c823a47704479a874dce57f4c51f0a95c5afd
Patrick Tobias <patrick.tobias@hotmail.com>
2024-08-02 20:30:31 -0300

    fix: CORS

--- lab-desafio_4-deploy/src/main/java/edu/dio/deploy/Application.java
+++ lab-desafio_4-deploy/src/main/java/edu/dio/deploy/Application.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+/*
+ * Erro CORS
+ * https://stackoverflow.com/questions/70843940/springdoc-openapi-ui-how-do-i-set-the-request-to-https
+ */
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL") })
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
